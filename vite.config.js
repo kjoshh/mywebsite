dde9cf12cb5f1c1560b008321979c7b2f0fad9ce
kjoshh <kernjoshua@gmx.de>
2025-03-08 11:36:13 +0100

    Update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,45 +1,3 @@
-// import { defineConfig } from 'vite';
-// import { resolve, dirname } from 'path'; // Import dirname
-// import { fileURLToPath } from 'url';
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
-// export default defineConfig({
-//   // root: resolve(__dirname),
-//   root: '.', // Set root to the current directory
-//   base: '/',
-//   rollupOptions: {
-//     input: {
-//       main: resolve(__dirname, 'index.html'),
-//       desktop: resolve(__dirname, 'desktop.html'),
-//       diary: resolve(__dirname, 'diary.html'),
-//       fuckMe: resolve(__dirname, 'fuck-me.html'),
-//       halloAsli: resolve(__dirname, 'hallo-asli.html'),
-//       howToLeaveTown1: resolve(__dirname, 'how-to-leave-town-p1.html'),
-//       howToLeaveTown2: resolve(__dirname, 'how-to-leave-town-p2.html'),
-//       howToLeaveTown3: resolve(__dirname, 'how-to-leave-town-p3.html'),
-//       howToLeaveTown5: resolve(__dirname, 'how-to-leave-town-p5.html'),
-//       howToLeaveTown6: resolve(__dirname, 'how-to-leave-town-p6.html'),
-//       howToLeaveTown4: resolve(__dirname, 'how-to-leave-town-p4.html'),
-//       howToLeaveTown7: resolve(__dirname, 'how-to-leave-town-p7.html'),
-//       howToLeaveTown8: resolve(__dirname, 'how-to-leave-town-p8.html'),
-//       howToLeaveTown9: resolve(__dirname, 'how-to-leave-town-p9.html'),
-//       howToLeaveTown: resolve(__dirname, 'how-to-leave-town.html'),
-//       loveMe: resolve(__dirname, 'love-me.html'),
-//       njePanimaju: resolve(__dirname, 'nje-panimaju.html'),
-//       rauber: resolve(__dirname, 'rauber.html'),
-//     },
-//     output: {
-//       assetFileNames: (assetInfo) => {
-//         if (assetInfo.name === 'src/js/htlt-fuck.js') {
-//           return 'src/js/htlt-fuck.js'; // Keep the original name and location
-//         }
-//         return 'assets/[name]-[hash][extname]'; // Default asset naming
-//       },
-//     },
-//   },
-// });
 import { defineConfig } from 'vite';
 import { resolve, dirname } from 'path';
 import { fileURLToPath } from 'url';
@@ -49,7 +7,7 @@ const __dirname = dirname(__filename);
 
 export default defineConfig({
   root: resolve(__dirname),
-  base: '/',
+  base: '/mywebsite/',
   build: {
     // Add this build configuration
     rollupOptions: {
